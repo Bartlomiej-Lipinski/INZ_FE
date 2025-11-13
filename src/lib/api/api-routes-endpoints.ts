6f198d4623afc6bc44afa6a6036fec78a31985e8
BL <109878606+Bartlomiej-Lipinski@users.noreply.github.com>
2025-11-13 23:22:08 +0100

    feature: add 2FA verification endpoint and integrate with client-side logic

--- src/lib/api/api-routes-endpoints.ts
+++ src/lib/api/api-routes-endpoints.ts
@@ -3,6 +3,7 @@ export const API_ROUTES = {
     REGISTER: '/api/auth/register',
     REFRESH: '/api/auth/refresh',
     USER_BY_ID: '/api/user',
+    VERIFY_2FA: '/api/auth/2FA',
     USER_GROUPS: '/api/user/groups',
     ADD_GROUP: '/api/group',
 } as const; 
