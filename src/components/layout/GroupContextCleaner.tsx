70773cd3423e33b6dfce30438ba54142ea80e0ad
ZvzaW <zuzia.wilkosz@op.pl>
2025-11-14 17:56:11 +0100

    feature: add GroupContextCleaner component to reset current group state based on pathname

---
+++ src/components/layout/GroupContextCleaner.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import { useGroupContext } from '@/contexts/GroupContext';
+
+
+export default function GroupContextCleaner() {
+  const pathname = usePathname();
+  const { setCurrentGroup, currentGroup } = useGroupContext();
+
+  useEffect(() => {
+    if (!pathname?.startsWith('/group-menu') && currentGroup !== null) {
+      setCurrentGroup(null);
+    }
+  }, [pathname, currentGroup, setCurrentGroup]);
+
+  return null;
+}
+
