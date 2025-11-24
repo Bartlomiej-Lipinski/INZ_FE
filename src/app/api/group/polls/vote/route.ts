cdd985c4010ec08a1f83aac25603f378bed9925d
BL <109878606+Bartlomiej-Lipinski@users.noreply.github.com>
2025-11-24 01:16:32 +0100

    feat: add API endpoints for managing polls and reactions with error handling

---
+++ src/app/api/group/polls/vote/route.ts
@@ -0,0 +1,34 @@
+import {NextRequest, NextResponse} from "next/server";
+import {fetchWithAuth} from "@/lib/api/fetch-with-auth";
+
+const BASE_URL = process.env.BASE_URL;
+const REACTIONS_POST = process.env.REACTIONS_POST;
+
+export async function POST(request: NextRequest) {
+    try {
+        const {entityType, groupId, targetId} = await request.json();
+        const endpoint = REACTIONS_POST?.replace('{groupId}', groupId)
+            .replace('{targetId}', targetId)
+            .replace('{entityType}', entityType);
+        const cookieHeader = request.headers.get('cookie') ?? '';
+        const response = await fetchWithAuth(`${BASE_URL}${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'Cookie': cookieHeader,
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        const data = await response.json();
+
+
+        return NextResponse.json(data, {status: response.status});
+    } catch (error) {
+        console.error('Comment creation API error:', error);
+        return NextResponse.json(
+            {success: false, message: 'Wystąpił błąd połączenia'},
+            {status: 500}
+        );
+    }
+}
--- src/app/api/reactions/get/route.ts
+++ src/app/api/reactions/get/route.ts
@@ -2,13 +2,13 @@ import {NextRequest, NextResponse} from "next/server";
 import {fetchWithAuth} from "@/lib/api/fetch-with-auth";
 
 const BASE_URL = process.env.BASE_URL;
-const GET_POST_COMMENTS = process.env.GET_POST_COMMENTS;
+const REACTIONS_GET = process.env.REACTIONS_GET;
 
 export async function GET(request: NextRequest) {
     try {
         const groupId = request.nextUrl.searchParams.get('groupId');
         const targetId = request.nextUrl.searchParams.get('targetId');
-        if (!GET_POST_COMMENTS) {
+        if (!REACTIONS_GET) {
             return NextResponse.json({success: false, message: 'Konfiguracja serwera niepełna'}, {status: 500});
         }
         if (!groupId || !targetId) {
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
                 message: 'Brak wymaganych parametrów: groupId lub targetId'
             }, {status: 400});
         }
-        const endpoint = GET_POST_COMMENTS?.replace('{groupId}', groupId)
+        const endpoint = REACTIONS_GET?.replace('{groupId}', groupId)
             .replace('{targetId}', targetId);
         const cookieHeader = request.headers.get('cookie') ?? '';
         const response = await fetchWithAuth(`${BASE_URL}${endpoint}`, {
@@ -28,42 +28,10 @@ export async function GET(request: NextRequest) {
             },
             credentials: 'include',
         });
-
         const data = await response.json();
-
-
         return NextResponse.json(data, {status: response.status});
     } catch (error) {
-        console.error('Comments retrieval API error:', error);
-        return NextResponse.json(
-            {success: false, message: 'Wystąpił błąd połączenia'},
-            {status: 500}
-        );
-    }
-}
-
-export async function POST(request: NextRequest) {
-    try {
-        const {entityType, content, groupId, targetId} = await request.json();
-        const endpoint = GET_POST_COMMENTS?.replace('{groupId}', groupId)
-            .replace('{targetId}', targetId);
-        const cookieHeader = request.headers.get('cookie') ?? '';
-        const response = await fetchWithAuth(`${BASE_URL}${endpoint}`, {
-            method: 'POST',
-            headers: {
-                'Cookie': cookieHeader,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ entityType, content }),
-            credentials: 'include',
-        });
-
-        const data = await response.json();
-
-
-        return NextResponse.json(data, {status: response.status});
-    } catch (error) {
-        console.error('Comment creation API error:', error);
+        console.error('reaction retrieval API error:', error);
         return NextResponse.json(
             {success: false, message: 'Wystąpił błąd połączenia'},
             {status: 500}
