58144fc3b1d5941717f7e3259d707ee66abbf959
BL <109878606+Bartlomiej-Lipinski@users.noreply.github.com>
2026-01-19 01:09:48 +0100

    fix: remove unused import of Grid2 component in availability calendar

--- src/app/group-events/availabilityCalendar-component.tsx
+++ src/app/group-events/availabilityCalendar-component.tsx
@@ -2,7 +2,6 @@
 
 import React, {useState} from 'react';
 import {Box, Button, Card, Chip, Grid, IconButton, Paper, Typography,} from '@mui/material';
-// import Grid2 from '@mui/material/Unstable_Grid2';
 import {ChevronLeft, ChevronRight} from 'lucide-react';
 import {
     addWeeks,
